c64b25f862dd249301d4f460d8e061d35e8ed7f8
Patrick Rose <pjr0911025+github@googlemail.com>
2013-07-31 11:11:58 +0100

    Added a setup file and a slightly easier way of installing - needs a bit of work

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='abc-transpose',
+      version='0.2',
+      description='abc transposer',
+      author='Patrick Rose',
+      author_email='pjr0911025@googlemail.com',
+      url='https://github.com/PatrickRose/abc-tranpose',
+      py_modules=['abc-transpose'],
+      package_dir={'': 'src'}
+      )
+
