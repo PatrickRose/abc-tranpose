c53ef623427e111b70f8aeeb1c5bd8b8549c8d84
Patrick Rose <pjr0911025+github@googlemail.com>
2013-08-02 11:24:41 +0100

    Fixed the transposing issues for semi-tones

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(name='abc-transpose',
-      version='0.3',
+      version='0.4',
       description='abc transposer',
       author='Patrick Rose',
       author_email='pjr0911025@googlemail.com',
       url='https://github.com/PatrickRose/abc-tranpose',
       py_modules=['abc-transpose'],
-      package_dir={'': 'src'}
+      package_dir={'': 'src'},
       )
 
